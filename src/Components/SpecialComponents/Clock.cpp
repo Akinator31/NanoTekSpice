8a0b82a4b7f37b589972346d4576a8e07b4a1c4f
Akinator31 <pavel.de-wavrechin@epitech.eu>
2026-02-12 12:18:12 +0100

    fix(clock): fix incorrect logic in clock component

--- src/Components/SpecialComponents/Clock.cpp
+++ src/Components/SpecialComponents/Clock.cpp
@@ -6,37 +6,30 @@
 #include "Errors/NanoTekSpiceErrors.h++"
 #include "Utils/Utils.h++"
 
-namespace nts
-{
-    Tristate Clock::getValue() const
-    {
+namespace nts {
+    Tristate Clock::getValue() const {
         return this->_currentState;
     }
 
-    void Clock::setValue(const Tristate value)
-    {
+    void Clock::setValue(const Tristate value) {
         this->_nextState = value;
     }
 
-    void Clock::simulate([[maybe_unused]] size_t tick)
-    {
-        // Quand on doit set la value d'une clock, on invert aussi ou on fait juste le set ?
-        if (this->_currentState == this->_nextState)
-        {
+    void Clock::simulate([[maybe_unused]] size_t tick) {
+        if (this->_currentState == this->_nextState) {
             this->_currentState = !this->_currentState;
+            this->_nextState = this->_currentState;
             return;
         }
-        this->_currentState = _nextState;
+        this->_currentState = !this->_nextState;
+        this->_nextState = this->_currentState;
     }
 
-    Tristate Clock::compute([[maybe_unused]] size_t pin)
-    {
+    Tristate Clock::compute([[maybe_unused]] size_t pin) {
         return this->_currentState;
     }
 
-    void Clock::setLink(const size_t pin, [[maybe_unused]] IComponent& other,
-                        [[maybe_unused]] size_t otherPin)
-    {
+    void Clock::setLink(const size_t pin, [[maybe_unused]] IComponent& other, [[maybe_unused]] size_t otherPin) {
         if (pin != 1)
             throw NanoTekSpiceException(SyntaxFileException);
     }
