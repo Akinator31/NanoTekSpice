a03da2198fdc9ff8764f04fdd6824645fcdb2af8
Nogo4 <noah.gomes@epitech.eu>
2026-02-12 11:31:14 +0100

    fix(true/false): on setlink, compute other pin to set at the correct state

--- src/Components/SpecialComponents/False.cpp
+++ src/Components/SpecialComponents/False.cpp
@@ -4,22 +4,20 @@
 
 #include "False.h++"
 #include "Errors/NanoTekSpiceErrors.h++"
+#include "IComponent.h++"
 
-namespace nts
-{
-    void False::simulate([[maybe_unused]] size_t tick)
-    {
+namespace nts {
+    void False::simulate([[maybe_unused]] size_t tick) {
     }
 
-    Tristate False::compute([[maybe_unused]] size_t pin)
-    {
+    Tristate False::compute([[maybe_unused]] size_t pin) {
         return this->_currentState;
     }
 
-    void False::setLink(const size_t pin, [[maybe_unused]] IComponent& other,
-                        [[maybe_unused]] size_t otherPin)
-    {
+    void
+    False::setLink(const size_t pin, [[maybe_unused]] IComponent& other, [[maybe_unused]] size_t otherPin) {
         if (pin != 1)
             throw NanoTekSpiceException(SyntaxFileException);
+        other.compute(otherPin);
     }
-}
+} // namespace nts
